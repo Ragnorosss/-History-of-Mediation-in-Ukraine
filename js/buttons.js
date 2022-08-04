163208146ebe05bd7f5134c1162e59c1206d4f82
Игорь Козловский <i.kozlovsky2004@gmail.com>
2022-08-04 14:52:13 +0300

    Revert "Merge pull request #2 from Ragnorosss/main"
    This reverts commit dd64682dc813f944906ae1f436e4ea6c62db5353, reversing
    changes made to 27c7b0ccad1cebf553045cd7c89bdd93c8f6ed55.

---
+++ js/buttons.js
@@ -0,0 +1,20 @@
+let btnContainer = document.getElementById("btn");
+let targetTextContainers = document.querySelectorAll(".target");
+let btns = [];
+for (let i = 0; i < btnContainer.children.length; i++) {
+  btns.push(btnContainer.children[i]);
+}
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", onClickToggleInfo);
+}
+let previousId = 0;
+function onClickToggleInfo(e) {
+  let id = btns.indexOf(e.target);
+  if (previousId !== id) {
+    btnContainer.querySelector(".active").classList.remove("active");
+    targetTextContainers[previousId].classList.add("hidden");
+    targetTextContainers[id].classList.remove("hidden");
+    e.target.classList.toggle("active");
+  }
+  previousId = id;
+}
