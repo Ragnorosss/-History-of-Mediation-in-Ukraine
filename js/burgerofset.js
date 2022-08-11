29d030c997583f341e51fd63abe4d9b7743e37bb
Ragnorosss <gost45678@gmail.com>
2022-08-11 20:31:25 +0300

    d

--- js/burgerofset.js
+++ js/burgerofset.js
@@ -3,7 +3,7 @@ let burgerOffset = document.querySelector('.menu__box')
 
    burgerOffset.addEventListener('wheel', function(){
       console.log(burgerOffset.scrollTop)
-      if (burgerOffset.scrollTop > 50 ) { 
+      if (burgerOffset.scrollTop > 50git  ) { 
          offsetHeader.classList.add('index__hidden')
       }else{
          offsetHeader.classList.remove('index__hidden')
