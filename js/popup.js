18a349c67c47497aeaec18a2e234106f9debd2c5
Ragnorosss <gost45678@gmail.com>
2022-08-11 19:49:49 +0300

    add popup menu

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+let popups = document.querySelector('.popup')
+
+let button_articles = document.querySelector('.more-info')
+
+button_articles.addEventListener('click', function(){ 
+   popups.classList.toggle('show__popups')
+})
