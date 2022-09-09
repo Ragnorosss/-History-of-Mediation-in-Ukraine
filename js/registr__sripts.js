967891cfee2c30613eeb15dd60000cb33e1be915
Ragnorosss <gost45678@gmail.com>
2022-09-10 00:01:29 +0300

    add files

---
+++ js/registr__sripts.js
@@ -0,0 +1,6 @@
+const buttonAuthor = document.querySelector('.header__button')
+const blockAuthor = document.querySelector('.registr ')
+
+buttonAuthor.addEventListener('click', function(){
+    blockAuthor.classList.toggle('hidden')
+})
