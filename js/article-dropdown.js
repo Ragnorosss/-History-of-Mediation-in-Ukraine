d5ca11dcd0e4195947b8ea80f98d4ee81ff1aa5f
Ragnorosss <gost45678@gmail.com>
2022-09-10 15:23:07 +0300

    add popup && fucn popup

--- js/article-dropdown.js
+++ js/article-dropdown.js
@@ -1,8 +1,15 @@
 const articlebutton = document.querySelector('.articles-items')
 const articleDropMenu = document.querySelector('.article__dropmenu')
+const buttonimg = document.querySelector('.dropmenu__btn-active ')
+const articlepopup = document.querySelector('.article__popup')
 
 articlebutton.addEventListener('click', function(e){
     articleDropMenu.classList.toggle('hidden')
 
    
+})
+
+buttonimg.addEventListener('click', function(e){
+    articlepopup.classList.toggle('hidden')
+
 })
