ae14503e9800cfd1118f25c1040836f536ee9fa5
Ragnorosss <gost45678@gmail.com>
2022-09-10 23:35:27 +0300

    change header

--- js/registr__scripts.js
+++ js/registr__scripts.js
@@ -2,5 +2,6 @@ const buttonAuthor = document.querySelector('.header__button')
 const blockAuthor = document.querySelector('.registr ')
 
 buttonAuthor.addEventListener('click', function(){
+    console.log("click!")
     blockAuthor.classList.toggle('hidden')
 })
