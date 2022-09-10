5a6b0bacc9d69e7e1aae2501172a6a4a4f81f9f3
Ragnorosss <gost45678@gmail.com>
2022-09-10 18:34:24 +0300

    add function js

--- js/article-dropdown.js
+++ js/article-dropdown.js
@@ -1,15 +1,30 @@
-const articlebutton = document.querySelector('.articles-items')
+const articlebutton = document.querySelector('.articles-items__item')
 const articleDropMenu = document.querySelector('.article__dropmenu')
 const buttonimg = document.querySelector('.dropmenu__btn-active ')
 const articlepopup = document.querySelector('.article__popup')
 
-articlebutton.addEventListener('click', function(e){
+
+articlebutton.addEventListener('click', function(){
     articleDropMenu.classList.toggle('hidden')
+    document.addEventListener('click', function (e) { 
+        if ( e.target !==  document.querySelector('.articles-items__item')) {
+            document.querySelector('.article__dropmenu').classList.add('hidden')
+            
+        }
+        else { 
+            document.querySelector('.article__dropmenu').classList.remove('hidden')
+        }
+    })
 
-   
 })
 
-buttonimg.addEventListener('click', function(e){
+buttonimg.addEventListener('click', function(){
     articlepopup.classList.toggle('hidden')
-
 })
+
+document.addEventListener('keydown', function (e) {
+    if ( e.key === 'Tab' || e.key === 'Escape' ) {
+        articleDropMenu.classList.add('hidden')
+    }
+})
+
