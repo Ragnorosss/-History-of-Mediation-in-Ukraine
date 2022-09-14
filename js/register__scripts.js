84bfc357d721342c806efcc26cc6e685b4393de6
Ragnorosss <gost45678@gmail.com>
2022-09-14 11:09:15 +0300

    change form

--- js/register__scripts.js
+++ js/register__scripts.js
@@ -1,5 +1,6 @@
 const buttonAuthor = document.querySelector('.header__button')
 const blockAuthor = document.querySelector('.registr ')
+const formRegistr = document.querySelector('.form__registr ')
 
 buttonAuthor.addEventListener('click', function(){
     blockAuthor.classList.toggle('hidden')
