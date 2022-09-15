f75495c2c836587e040a17ece65e33cf891088df
Ragnorosss <gost45678@gmail.com>
2022-09-15 16:01:46 +0300

    fix

--- js/article-dropdown.js
+++ js/article-dropdown.js
@@ -1,41 +1,36 @@
-const articlebutton = document.querySelector('.articles-items__item')
+const articlebutton = document.querySelectorAll('.articles-items__item')
 const articleDropMenu = document.querySelector('.article__dropmenu')
-
-const listsDropMenu = document.querySelectorAll('.dropmenu__lists')
-const listDropMenu = document.querySelectorAll('.dropmenu__list-item')
-const btnDropMenu = document.querySelector('.dropdown__switch')
 
 const buttonimg = document.querySelector('.dropmenu__btn-active ')
 const articlepopup = document.querySelector('.article__popup')
 
+let intervalId;
 
-articlebutton.addEventListener('click', function(){
-    articleDropMenu.classList.toggle('hidden')
-    /*document.addEventListener('click', function (e) { 
-        if ( e.target !==  document.querySelector('.articles-items__item')) {
-            document.querySelector('.article__dropmenu').classList.add('hidden')
-            
-        }
-        else { 
-            document.querySelector('.article__dropmenu').classList.remove('hidden')
-        }
-     })*/ 
+document.querySelectorAll('.dropdown__switch').forEach(e => {
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path;
+        document.querySelectorAll('.dropdown__inner').forEach(e => {
+            if (!document.querySelector(`[data-target=${menu}]`).classList.contains('hidden')) {
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.add('hidden');
+                }, 0);
+            }
 
-})
-btnDropMenu.addEventListener('click', function(){
-    listsDropMenu.forEach(listDropMenu => {
-        listDropMenu.classList.toggle('hidden')
-    });
-})
-
-function dropMenuLists () {
-    listsDropMenu.forEach(listDropMenu => {
-        listsDropMenu.forEach(btnDropMenu => {
-            listDropMenu.classList.toggle('hidden')
+            if (document.querySelector(`[data-target=${menu}]`).classList.contains('hidden')) {
+                clearTimeout(intervalId);
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('hidden');
+                }, 0);
+            }
         });
     });
-}
+});
 
+document.querySelectorAll('.articles-items').forEach(articlebutton => {
+    articlebutton.addEventListener('click', function(){
+        articleDropMenu.classList.toggle('hidden')
+    })
+});
 
 buttonimg.addEventListener('click', function(){
     articlepopup.classList.toggle('hidden')
