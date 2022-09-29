caf75c9af1760bbf418f2d23cb634fa2b734b8e8
Ragnorosss <gost45678@gmail.com>
2022-09-29 22:23:55 +0300

    fix css code

--- js/filters.js
+++ js/filters.js
@@ -1,4 +1,14 @@
-function scrollDown() { //scrolls down filter on click
-    var myDiv = document.getElementById("filters");
-    myDiv.scrollTop = myDiv.scrollHeight;
-}
+const openFiletr = document.querySelector('.filter__img')
+
+const filetrBar = document.querySelector('.filters')
+
+
+// function scrollDown() { //scrolls down filter on click
+//     var myDiv = document.getElementById("filters");
+//     myDiv.scrollTop = myDiv.scrollHeight;
+// }
+
+
+openFiletr.addEventListener('click' , function(){
+    filetrBar.classList.toggle('hidden')
+})
