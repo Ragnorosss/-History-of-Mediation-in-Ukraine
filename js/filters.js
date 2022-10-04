7da3bd4f98591a711c6fdfd981e52bf35d5a1c46
Ragnorosss <gost45678@gmail.com>
2022-10-04 10:32:06 +0300

    fix filterbar

--- js/filters.js
+++ js/filters.js
@@ -2,6 +2,7 @@ const openFiletr = document.querySelector('.filter__img')
 
 const filetrBar = document.querySelector('.filters')
 
+const chekWidthWindow = window.matchMedia('@media(max-width: 1100px)')
 
 // function scrollDown() { //scrolls down filter on click
 //     var myDiv = document.getElementById("filters");
@@ -9,6 +10,14 @@ const filetrBar = document.querySelector('.filters')
 // }
 
 
-openFiletr.addEventListener('click' , function(){
-    filetrBar.classList.toggle('hidden')
+
+function removeCssStyle () { 
+    if (chekWidthWindow < 1100) {
+        filetrBar.style.display = "none";
+    }
+}
+
+openFiletr.addEventListener('click', function(){
+        filetrBar.classList.toggle('hidden')
+        removeCssStyle()
 })
