4d3bb037d6df6e1f4cc3c92750e1deb97798a58b
Ragnorosss <gost45678@gmail.com>
2022-10-14 20:24:38 +0300

    add func to requsts js

--- js/requsts.js
+++ js/requsts.js
@@ -1,14 +1,22 @@
-let user = {
-    name: 'John',
-    surname: 'Smith'
+let author = {
+    name,
+    lastname,
+    email,
+    password,
+    quilification,
+    biography,
+    avatar,
+    city,
+    videos:URL(),
+     
   };
   
   let response = await fetch('/article/fetch/post/user', {
-    method: 'POST',
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify(user)
+    body: JSON.stringify(author)
   });
   
   let result = await response.json();
