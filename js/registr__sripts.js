5dab430563983520962d6358a9a2c641e685381e
Ragnorosss <gost45678@gmail.com>
2022-10-14 18:54:13 +0300

    rework js file

--- js/registr__sripts.js
+++ js/registr__sripts.js
@@ -4,11 +4,13 @@ const SignInBtn = document.querySelector('#sign-up')
 const formLogin = document.querySelector('.registr')
 const formSign = document.querySelector('.sign__in')
 
-const burgerSignin = document.querySelector('.signin')
-const burgerLogin = document.querySelector('.login')
 
 const formRegistr = document.querySelector('.form__registr ')
 const formSignin = document.querySelector('.form__signin ')
+
+
+const burgerSignin = document.querySelector('.signin')
+const burgerLogin = document.querySelector('.login')
 
 const submitBtn = document.querySelector('.submit__btn ')
 const submitBtnSignIn = document.querySelector('.submit__btn-signin')
@@ -18,6 +20,73 @@ const checkinput = document.querySelectorAll('.empty ')
 const checkRange = document.querySelectorAll('.range ')
 const errorEmpty = document.querySelectorAll('.error-empty')
 const errorPass = document.querySelectorAll('.error-password')
+
+const patternsPass = {
+    'number':     '0-9',
+    'special':     '!@#$%^&*',
+    'latin_lower': 'a-z',
+    'latin_upper': 'A-Z'
+  };
+
+function validPassSignin(event) {
+    event.preventDefault()
+    
+    for (let i = 0; i < checkinput.length; i++) {
+        if (!checkinput[i].value) {
+            errorEmpty.forEach(errorEmpty => {
+                document.querySelector('.submit__btn').addEventListener('click', function(){
+                    errorEmpty.classList.remove('hidden')
+                })
+            });
+        }
+    }
+
+      for (let i = 0; i < checkRange.length; i++) {
+        
+        if ( !formPassword != patternsPass ) { 
+                document.querySelector('.submit__btn').addEventListener('click',function(e){
+                document.querySelector('.error-password').classList.remove('hidden')
+            })
+        }
+       
+    }
+   
+}
+
+function validPassLogin(event){
+    event.preventDefault()
+    
+    for (let i = 0; i < checkinput.length; i++) {
+        if (!checkinput[i].value) {
+            errorEmpty.forEach(errorEmpty => {
+                submitBtnSignIn.addEventListener('click', function(){
+                    errorEmpty.classList.remove('hidden')
+                })
+            });
+        }
+    }
+
+ 
+
+      for (let i = 0; i < checkRange.length; i++) {
+        
+        if ( !formPassword != patternsPass ) { 
+            errorPass.forEach(errorPass => {
+                submitBtnSignIn.addEventListener('click', function(){
+                    errorPass.classList.remove('hidden')
+                })
+            });
+        }
+       
+    }
+}
+
+
+formSignin.addEventListener('submit', validPassLogin)
+   
+
+formRegistr.addEventListener('submit',validPassSignin ) 
+
 
 LoginBtn.addEventListener('click', function(){
     formLogin.classList.toggle('hidden')
@@ -33,64 +102,4 @@ burgerSignin.addEventListener('click', function(){
 
 burgerLogin.addEventListener('click', function(){
     formLogin.classList.toggle('hidden')
-})
-
-formRegistr.addEventListener('submit', function (event) {
-    event.preventDefault()
-    
-    for (let i = 0; i < checkinput.length; i++) {
-        if (!checkinput[i].value) {
-            errorEmpty.forEach(errorEmpty => {
-                document.querySelector('.submit__btn').addEventListener('click', function(){
-                    errorEmpty.classList.remove('hidden')
-                })
-            });
-        }
-    }
-
-    let patternsPass = {
-        'number':     '0-9',
-        'special':     '!@#$%^&*',
-        'latin_lower': 'a-z',
-        'latin_upper': 'A-Z'
-      };
-
-
-      for (let i = 0; i < checkRange.length; i++) {
-        
-        if ( !formPassword != patternsPass ) { 
-            document.querySelector('.submit__btn').addEventListener('click',function(e){
-                document.querySelector('.error-password').classList.remove('hidden')
-            })
-        }
-       
-    }
-   
-})
-
-formSignin.addEventListener('submit', function (event) {
-    event.preventDefault()
-    
-    for (let i = 0; i < checkinput.length; i++) {
-        if (!checkinput[i].value) {
-            errorEmpty.forEach(errorEmpty => {
-                submitBtnSignIn.addEventListener('click', function(){
-                    errorEmpty.classList.remove('hidden')
-                })
-            });
-        }
-    }
-
-      for (let i = 0; i < checkRange.length; i++) {
-        
-        if ( !formPassword != patternsPass ) { 
-            errorPass.forEach(errorPass => {
-                submitBtnSignIn.addEventListener('click', function(){
-                    errorPass.classList.remove('hidden')
-                })
-            });
-        }
-       
-    }
-   
 })
